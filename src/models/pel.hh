97f1fd118113c7d47278128c7780ac328980fa4a
Fernando Vilas <fvilas@iname.com>
2025-07-13 20:15:45 -0500

    Make PEL model constexpr

--- src/models/pel.hh
+++ src/models/pel.hh
@@ -1,6 +1,32 @@
+/*****************************************************************************
+ *  Plane Earth Path Loss model for Signal Server by Alex Farrant             *
+ *  Taken from "Antennas and Propagation for wireless communication systems"  *
+ *  ISBN 978-0-470-84879-1                                                    *
+ *  10 August 2016							     *
+ *  This program is free software; you can redistribute it and/or modify it   *
+ *  under the terms of the GNU General Public License as published by the     *
+ *  Free Software Foundation; either version 2 of the License or any later    *
+ *  version.								     *
+ *									     *
+ *  This program is distributed in the hope that it will useful, but WITHOUT  *
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     *
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License     *
+ *  for more details.							     *
+ *									     */
+
 #ifndef _PEL_HH_
 #define _PEL_HH_
 
-double PlaneEarthLoss(float d, float TxH, float RxH);
+#include <cmath>
+
+constexpr auto PlaneEarthLoss(float d, float TxH, float RxH) -> double {
+    /*
+     Plane Earth Loss model                                             *
+     Frequency: N/A
+     Distance (km): Any
+     */
+    // Plane earth loss is independent of frequency.
+    return 40*std::log10(d) + 20*std::log10(TxH) + 20*std::log10(RxH);
+}
 
 #endif /* _PEL_HH_ */
