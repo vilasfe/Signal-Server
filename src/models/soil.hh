12b5df50580c7140acf7e36f79d482234c0df2be
Fernando Vilas <fvilas@iname.com>
2025-07-13 20:23:06 -0500

    Make soil model constexpr

--- src/models/soil.hh
+++ src/models/soil.hh
@@ -1,6 +1,31 @@
+/*****************************************************************************
+ *  Soil Path Loss model for Signal Server by Alex Farrant                    *
+ *  21 February 2018                                                          *
+ *                                                                            *
+ *  This program is free software; you can redistribute it and/or modify it   *
+ *  under the terms of the GNU General Public License as published by the     *
+ *  Free Software Foundation; either version 2 of the License or any later    *
+ *  version.                                                                  *
+ *                                                                            *
+ *  This program is distributed in the hope that it will useful, but WITHOUT  *
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     *
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License     *
+ *  for more details.                                                         *
+ *
+ * Frequency: Any MHz
+ * Distance: Any Km
+ * Terrain permittivity: 1 - 15 (Bad to Good)
+ */
+
 #ifndef _SOIL_HH_
 #define _SOIL_HH_
 
-double SoilPathLoss(float f, float d, float t);
+#include "../common.h"
+
+constexpr auto SoilPathLoss(float f, float d, float terdic) -> double
+{
+    const auto soil = (120/terdic);
+    return(6.4 + _20log10f(d) + _20log10f(f)+(8.69*soil));
+}
 
 #endif /* _SOIL_HH_ */
