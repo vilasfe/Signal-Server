7d12e53ca86f10ab711a5b23118d8401853c968c
Fernando Vilas <fvilas@iname.com>
2026-02-02 19:03:20 -0600

    Make CTest work

---
+++ src/test_main.cc
@@ -0,0 +1,8 @@
+// Do this manually since gtest recommends it
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
